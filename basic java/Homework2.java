af4c4ba06676ff91c1898e63440cfaaedacae6f1
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-01-10 21:21:17 +0530

    day 8 of learing java

--- basic java/Homework2.java
+++ basic java/Homework2.java
@@ -10,55 +10,55 @@ public class Homework2 {
 
         // * Print hollow Butterfly
         // upper half
-        // for (int i = 0; i <= rows; i++) {
+        for (int i = 0; i <= rows; i++) {
 
-        // for (int j = 1; j <= i; j++) {
-        // if (j == 1 || j == i) {
-        // System.out.print("*");
-        // } else {
-        // System.out.print(" ");
-        // }
-        // }
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
 
-        // for (int space = 1; space <= 2 * (rows - i); space++) {
-        // System.out.print(" ");
-        // }
+            for (int space = 1; space <= 2 * (rows - i); space++) {
+                System.out.print(" ");
+            }
 
-        // for (int k = 1; k <= i; k++) {
-        // if (k == 1 || k == i) {
-        // System.out.print("*");
-        // } else {
-        // System.out.print(" ");
-        // }
-        // }
+            for (int k = 1; k <= i; k++) {
+                if (k == 1 || k == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
 
-        // System.out.println();
-        // }
-        // // Lower Half
-        // for (int i = rows; i >= 1; i--) {
+            System.out.println();
+        }
+        // Lower Half
+        for (int i = rows; i >= 1; i--) {
 
-        // for (int j = 1; j <= i; j++) {
-        // if (j == 1 || j == i) {
-        // System.out.print("*");
-        // } else {
-        // System.out.print(" ");
-        // }
-        // }
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
 
-        // for (int space = 1; space <= 2 * (rows - i); space++) {
-        // System.out.print(" ");
-        // }
+            for (int space = 1; space <= 2 * (rows - i); space++) {
+                System.out.print(" ");
+            }
 
-        // for (int k = 1; k <= i; k++) {
-        // if (k == 1 || k == i) {
-        // System.out.print("*");
-        // } else {
-        // System.out.print(" ");
-        // }
-        // }
+            for (int k = 1; k <= i; k++) {
+                if (k == 1 || k == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
 
-        // System.out.println();
-        // }
+            System.out.println();
+        }
 
         // * Print hollow Rhombus
 
