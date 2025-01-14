89e4523fc12f40228d47fd1d67b1e2237c22b1e0
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-01-14 21:00:17 +0530

    some changes

--- basic java/Array2d.java
+++ basic java/Array2d.java
@@ -1,6 +1,36 @@
+import java.util.*;
+
 public class Array2d {
     public static void main(String argu[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("2D Array In Java ");
-        int[][] array2d = new int[3][5];
+        int[][] numbers = new int[3][5];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // * Take array as a input from user and search the element given element
+
+        System.out.print("Enter a number you want to search: ");
+        int search = sc.nextInt();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (numbers[i][j] == search) {
+                    System.out.print("Index is: " + "(" + i + "," + j + ")");
+                }
+            }
+        }
+
     }
 }
