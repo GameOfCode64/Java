dcf9532e81cfca94dd25ae318dea169d25bc0c02
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-01-20 22:13:46 +0530

    Day 17 of Learning Java

--- basic java/Sorting.java
+++ basic java/Sorting.java
@@ -13,6 +13,32 @@ public class Sorting {
 
     }
 
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallestIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallestIdx] > arr[j]) {
+                    smallestIdx = j;
+                }
+            }
+            int temp = arr[smallestIdx];
+            arr[smallestIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length - 1; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+    }
+
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -20,9 +46,11 @@ public class Sorting {
     }
 
     public static void main(String argu[]) {
-        int arr[] = { 7, 3, 5, 2, 1, 4, 6 };
+        int arr[] = { 3, 5, 2, 1, 4, 6 };
 
         bubbleSort(arr);
+        selectionSort(arr);
+        insertionSort(arr);
         printArray(arr);
     }
 }
