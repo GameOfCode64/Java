b5d99cefaf520905d78a9206f67e69e08c080fba
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-01-22 22:15:31 +0530

    Day 19 of Learning JAVA

--- basic java/recursion.java
+++ basic java/recursion.java
@@ -37,10 +37,52 @@ public class recursion {
         }
     }
 
+    public static int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static int calcPower(int x, int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+
+        if (x == 0) {
+            return 0;
+        }
+
+        int xPower = calcPower(x, n - 1);
+        int xPowr = x * xPower;
+        return xPowr;
+    }
+
+    public static int calcPower1(int x, int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+
+        if (x == 0) {
+            return 0;
+        }
+
+        if (n % 2 == 0) {
+            return calcPower1(x, n / 2) * calcPower1(x, n / 2);
+        } else {
+            return calcPower1(x, n / 2) * calcPower1(x, n / 2) * x;
+        }
+
+    }
+
     public static void main(String argu[]) {
         printNumber(5);
         printNum(1);
         System.out.println(printSum(5));
         System.out.println(Factorial(5));
+        System.out.println(fibonacci(5));
     }
 }
