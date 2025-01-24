0624371777cd8417e0346ce3cc72154cd85062c1
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-01-24 22:08:31 +0530

    Day 21 of Learning JAVA

--- basic java/recursion2.java
+++ basic java/recursion2.java
@@ -1,5 +1,18 @@
 public class recursion2 {
-    public static void main(String argu[]) {
 
+    public static void towerOfHanoi(int n, String src, String helper, String dest) {
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " From " + src + " to " + dest);
+            return;
+        }
+
+        towerOfHanoi(n - 1, src, dest, helper);
+        System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+        towerOfHanoi(n - 1, helper, src, dest);
+    }
+
+    public static void main(String argu[]) {
+        int n = 3;
+        towerOfHanoi(n, "S", "H", "D");
     }
 }
