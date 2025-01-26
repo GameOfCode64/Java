f4ef61930ea4c28102a3254e8e7ed362043fb097
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-01-26 21:58:50 +0530

    changes in recursion 4

--- basic java/recursion4.java
+++ basic java/recursion4.java
@@ -1,6 +1,26 @@
 public class recursion4 {
+
+    public static boolean[] map = new boolean[26];
+
+    public static void removeDuplicates(String str, int indX, String newString) {
+        if (indX == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(indX);
+
+        if (map[currChar - 'a']) {
+            removeDuplicates(str, indX + 1, newString);
+        } else {
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, indX + 1, newString);
+        }
+    }
+
     public static void main(String argu[]) {
         System.out.println("Day 4 of Recursion in Java");
-
+        String str = "aabcbdc";
+        removeDuplicates(str, 0, "");
     }
 }
