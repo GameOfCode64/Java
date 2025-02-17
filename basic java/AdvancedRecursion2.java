e0cbf841b507a5b00ee57eeade533c7568ea0226
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-02-17 17:39:55 +0530

    day 23 of learing java

--- basic java/AdvancedRecursion2.java
+++ basic java/AdvancedRecursion2.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class AdvancedRecursion2 {
 
     public static int placeTiles(int n, int m) {
@@ -17,9 +19,56 @@ public class AdvancedRecursion2 {
         return vertically + horizontally;
     }
 
+    public static int callGuests(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        // single
+        int ways1 = callGuests(n - 1);
+
+        // pair
+        int ways2 = (n - 1) * callGuests(n - 2);
+        return ways1 + ways2;
+    }
+
+    public static void printSubset(ArrayList<Integer> subset) {
+        for (int i = 0; i < subset.size(); i++) {
+            System.out.print(subset.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void findSubSets(int n, ArrayList<Integer> subset) {
+
+        if (n == 0) {
+            printSubset(subset);
+            return;
+        }
+
+        // add in subset
+        subset.add(n);
+        findSubSets(n - 1, subset);
+
+        // remove in subset
+        subset.remove(subset.size() - 1);
+        findSubSets(n - 1, subset);
+    }
+
     public static void main(String[] args) {
+
+        ArrayList<Integer> subset = new ArrayList<>();
+
         // * Place Tiles of Size 1xm in a floor of size nxm
+
         int n = 4, m = 2;
         System.out.println(placeTiles(n, m));
+
+        // * Find the number of ways in which you can invite n people to your party or
+        // * in pairs.
+        System.out.println(callGuests(n));
+
+        // * print all subsets of a set of first n natural number
+        findSubSets(n, subset);
     }
 }
